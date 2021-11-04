e3207693e482a0e45c85ef96a2ac035d80c40c1b
Marta Obregón <mor683@alumnos.unican.es>
2021-11-04 18:21:06 +0100

    Añadir clases en SupermercadoBusiness

---
+++ practica03/SupermercadoBusiness/src/main/java/es/unican/ps/supermercado/negocio/GestionArticulos.java
@@ -0,0 +1,5 @@
+package es.unican.ps.supermercado.negocio;
+
+public class GestionArticulos implements IGestionArticulosLocal, IGestionArticulosRemote {
+
+}
---
+++ practica03/SupermercadoBusiness/src/main/java/es/unican/ps/supermercado/negocio/GestionUsuarios.java
@@ -0,0 +1,5 @@
+package es.unican.ps.supermercado.negocio;
+
+public class GestionUsuarios implements IGestionUsuariosLocal, IGestionUsuariosRemote {
+
+}
