53bc40e32a769bb2c7966007e40e935ec873bfe9
Marta Obregón <mor683@alumnos.unican.es>
2021-11-08 11:39:20 +0100

    Añadir implementación de las clases de negocio GestionArticulos y GestionUsuarios

--- practica03/SupermercadoBusiness/src/main/java/es/unican/ps/supermercado/negocio/GestionArticulos.java
+++ practica03/SupermercadoBusiness/src/main/java/es/unican/ps/supermercado/negocio/GestionArticulos.java
@@ -1,5 +1,32 @@
 package es.unican.ps.supermercado.negocio;
 
+import javax.ejb.EJB;
+import javax.ejb.Stateless;
+
+import es.unican.ps.supermercado.negocio.dominio.Articulo;
+import es.unican.ps.supermercado.negocio.interfaces.IArticulosDAO;
+import es.unican.ps.supermercado.negocio.interfaces.IGestionArticulosLocal;
+import es.unican.ps.supermercado.negocio.interfaces.IGestionArticulosRemote;
+
+@Stateless
 public class GestionArticulos implements IGestionArticulosLocal, IGestionArticulosRemote {
+
+	@EJB
+	private IArticulosDAO articulosDAO;
+
+	public Articulo anhadirArticulo(Articulo a) {
+		return articulosDAO.crearArticulo(a);
+	}
+
+	public Articulo eliminarArticulo(Articulo a) {
+		return articulosDAO.eliminarArticulo(a);
+	}
+
+	public Articulo actualizarStock(Articulo a, int unidades) {
+		if(unidades >= 0) {
+			a.setUnidadesStock(unidades);
+		}
+		return articulosDAO.modificarArticulo(a);
+	}
 
 }
--- practica03/SupermercadoBusiness/src/main/java/es/unican/ps/supermercado/negocio/GestionUsuarios.java
+++ practica03/SupermercadoBusiness/src/main/java/es/unican/ps/supermercado/negocio/GestionUsuarios.java
@@ -1,5 +1,27 @@
 package es.unican.ps.supermercado.negocio;
 
+import javax.ejb.EJB;
+
+import es.unican.ps.supermercado.negocio.dominio.Usuario;
+import es.unican.ps.supermercado.negocio.interfaces.IGestionUsuariosLocal;
+import es.unican.ps.supermercado.negocio.interfaces.IGestionUsuariosRemote;
+import es.unican.ps.supermercado.negocio.interfaces.IUsuariosDAO;
+
 public class GestionUsuarios implements IGestionUsuariosLocal, IGestionUsuariosRemote {
+
+	@EJB
+    private IUsuariosDAO usuariosDAO;
+	
+	public Usuario registrar(Usuario u) {
+		return usuariosDAO.crearUsuario(u);
+	}
+
+	public boolean login(String dni) {
+		boolean loginCorrecto = false;
+		if (usuariosDAO.usuarioPorDni(dni) != null) {
+			loginCorrecto = true;
+		}
+		return loginCorrecto;
+	}
 
 }
