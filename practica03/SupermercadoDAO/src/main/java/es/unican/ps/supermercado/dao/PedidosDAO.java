70c919ea24100b001cfaac486818f5328fd887d8
Marta Obregón <mor683@alumnos.unican.es>
2021-12-05 17:10:38 +0100

    Añadir implemetacion PedidosDAO

--- practica03/SupermercadoDAO/src/main/java/es/unican/ps/supermercado/dao/PedidosDAO.java
+++ practica03/SupermercadoDAO/src/main/java/es/unican/ps/supermercado/dao/PedidosDAO.java
@@ -1,5 +1,68 @@
 package es.unican.ps.supermercado.dao;
 
-public class PedidosDAO {
+import java.util.Set;
 
+import es.unican.ps.supermercado.negocio.dominio.Articulo;
+import es.unican.ps.supermercado.negocio.dominio.Pedido;
+
+@Stateless
+public class PedidosDAO implements IPedidosDAOLocal, IPedidosDAORemote {
+
+	@PersistenceContext(unitName="supermercadoPU")
+	private EntityManager em;
+
+	@Override
+	public Pedido crearPedido(Pedido p) {
+		// Hacemos persistente el pedido
+		try {
+			em.persist(p);
+		} catch (EntityExistsException e) {
+			// Ya existe un pedido en la BD con esa referencia
+			return null;
+		}
+		return p;
+	}
+
+	@Override
+	public Pedido modificarPedido(Pedido p) {
+		// Modificamos el pedido
+		try {
+			em.merge(p);
+		} catch (EntityExistsException e) {
+			// No existe el pedido
+			return null;
+		}
+		return p;
+	}
+
+	@Override
+	public Pedido eliminarPedido(Pedido p) {
+		// Eliminamos el pedido
+		try {
+			em.remove(p);
+		} catch (EntityExistsException e) {
+			// No existe el pedido
+			return null;
+		}
+		return p;
+	}
+
+	@Override
+	public Pedido pedidoPorReferencia(String ref) {
+		Pedido p;
+		// Buscamos el pedido con la referencia indicada
+		try {
+			p = em.find(Pedido.class, nombre);
+		} catch (EntityExistsException e) {
+			// No existe el pedido
+			return null;
+		}
+		return p;
+	}
+
+	@Override
+	public Set<Pedido> pedidos() {
+		Query q = em.createQuery("SELECT p from Pedido p");
+		return (Set<Pedido>) q.getResultList();
+	}
 }
